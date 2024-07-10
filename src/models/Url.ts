45e3c1f16bbb9e75d68a14986f502ff0adf0b518
abhikatta <abhinaykatta97@gmail.com>
2024-07-10 14:15:34 +0530

    url shortner app initalize

---
+++ src/models/Url.ts
@@ -0,0 +1,25 @@
+import { Document, model, Model, models, Schema } from "mongoose";
+
+const urlSchema = new Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    shortUrl: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export interface IUrl extends Document {
+  originalUrl: string;
+  shortUrl: string;
+}
+const Url: Model<IUrl> = models.Url || model<IUrl>("Url", urlSchema);
+
+export default Url;
